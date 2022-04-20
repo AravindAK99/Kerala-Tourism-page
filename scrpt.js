ee26bf573826a8e8d49165b04615b3c5abf0e45c
AravindAK99 <102467646+AravindAK99@users.noreply.github.com>
2022-04-20 15:01:32 +0530

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -105,3 +105,13 @@ alert("account creation successful")
 
 
 
+
+function myFunction() {
+      var x = document.getElementById("confirmpassword");
+      if (x.type === "password") {
+        x.type = "text";
+      } else {
+        x.type = "password";
+      }
+    }
+
